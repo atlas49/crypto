400b01a4237eb94de82fb4029a4b08871f3baa60
Serhii <serhii.p@code-care.pro>
2023-09-25 14:03:55 +0300

    Add pages

--- src/pages/Partners/index.tsx
+++ src/pages/Partners/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const PartnersPage = () => {
   return <div>PartnersPage</div>;
 };
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import Wrapper from '@components/Layout/Wrapper';
 
 import Faq from '@pages/Faq';
@@ -8,55 +8,58 @@ import Privacy from '@pages/Privacy';
 import Partners from '@pages/Partners';
 import Guarantees from '@pages/Guarantees';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <Wrapper>
-        <Main />
-      </Wrapper>
-    ),
-  },
-  {
-    path: '/faq',
-    element: (
-      <Wrapper>
-        <Faq />
-      </Wrapper>
-    ),
-  },
-  {
-    path: '/support',
-    element: (
-      <Wrapper>
-        <Support />
-      </Wrapper>
-    ),
-  },
-  {
-    path: '/privacy',
-    element: (
-      <Wrapper>
-        <Privacy />
-      </Wrapper>
-    ),
-  },
-  {
-    path: '/partners',
-    element: (
-      <Wrapper>
-        <Partners />
-      </Wrapper>
-    ),
-  },
-  {
-    path: '/guarantees',
-    element: (
-      <Wrapper>
-        <Guarantees />
-      </Wrapper>
-    ),
-  },
-]);
+const router = createHashRouter(
+  [
+    {
+      path: '/crypto',
+      element: (
+        <Wrapper>
+          <Main />
+        </Wrapper>
+      ),
+    },
+    {
+      path: '/faq',
+      element: (
+        <Wrapper>
+          <Faq />
+        </Wrapper>
+      ),
+    },
+    {
+      path: '/support',
+      element: (
+        <Wrapper>
+          <Support />
+        </Wrapper>
+      ),
+    },
+    {
+      path: '/privacy',
+      element: (
+        <Wrapper>
+          <Privacy />
+        </Wrapper>
+      ),
+    },
+    {
+      path: '/partners',
+      element: (
+        <Wrapper>
+          <Partners />
+        </Wrapper>
+      ),
+    },
+    {
+      path: '/guarantees',
+      element: (
+        <Wrapper>
+          <Guarantees />
+        </Wrapper>
+      ),
+    },
+  ],
+  { basename: '/' }
+);
 
 export default router;
