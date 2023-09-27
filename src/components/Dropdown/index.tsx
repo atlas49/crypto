0ea40cbfcd8325d7b4f49bc8a71a65985940bd62
Serhii <serhii.p@code-care.pro>
2023-09-27 14:10:20 +0300

    Fix unused vars

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,30 +1,16 @@
-import React, { ChangeEvent } from 'react';
-import { Theme, useTheme } from '@mui/material/styles';
+import React from 'react';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 
 import { Currency } from '../../state/context';
-import style from './style.module.scss';
 
 interface IDropdown {
   data: Currency[];
   onChange: (value: string) => void;
   selectedData: Currency | null;
 }
-
-// const Dropdown: React.FC<IDropdown> = ({ data, onChange, selectedData }) => {
-//   return (
-//     <select onChange={onChange} value={selectedData?.code} className={style.select}>
-//       {data.map((curr) => (
-//         <option key={curr.code} value={curr.code}>
-//           {curr.code}
-//         </option>
-//       ))}
-//     </select>
-//   );
-// };
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
